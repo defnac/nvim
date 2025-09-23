f0e45a713df8590528ca5c8430684c1313d34eee
defnac <daniel.fernandez.es@gmail.com>
2025-09-23 21:33:35 +0200

    updated lsp config handling for 0.11

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,10 +1,23 @@
 return {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-        ensure_installed = {"ts_ls", "omnisharp"},
+        ensure_installed = { "lua_ls", "ts_ls", "omnisharp" },
     },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
         "neovim/nvim-lspconfig",
-    }
+    },
+    config = function()
+        require("mason-lspconfig").setup {
+            automatic_enable = true,
+        }
+
+        local servers = { "lua_ls" } -- list of servers you want to override
+        for _, name in ipairs(servers) do
+            local ok, conf = pcall(require, "lsp." .. name)
+            if ok then
+                vim.lsp.config(name, conf)
+            end
+        end
+    end
 }

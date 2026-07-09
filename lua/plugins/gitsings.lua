7f951973a32db50e22e64895c27ce98fa9c42e32
DaniPCF <daniel.fernandez@catchalot.com>
2026-07-09 16:23:41 +0200

    add: git preview

--- lua/plugins/gitsings.lua
+++ lua/plugins/gitsings.lua
@@ -3,6 +3,9 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   keys = {
     { '<leader>gb', function() require('gitsigns').blame_line({ full = true }) end, desc = 'Git blame line' },
+    { '<leader>gp', function() require('gitsigns').preview_hunk() end, desc = 'Preview git hunk (diff)' },
+    { ']c', function() require('gitsigns').next_hunk() end, desc = 'Next git hunk' },
+    { '[c', function() require('gitsigns').prev_hunk() end, desc = 'Previous git hunk' },
   },
   opts = {
     signs = {

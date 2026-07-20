e32352b0811dfddcc96c01b5d268992f2a4009d9
DaniPCF <daniel.fernandez@catchalot.com>
2026-07-20 15:42:59 +0200

    add: bqf and harpoon

---
+++ lua/plugins/bqf.lua
@@ -0,0 +1,9 @@
+return {
+    "kevinhwang91/nvim-bqf",
+    ft = "qf", -- only loads when the quickfix filetype opens — stays out of your way otherwise
+    opts = {
+        preview = {
+            winblend = 0,
+        },
+    },
+}
